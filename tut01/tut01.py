979d63505441c4fa3b6093a934870075b686058f
ovd0312 <ovd0312@gmail.com>
2021-08-18 23:07:51 +0530

    Added Solution for Tut1

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,29 @@
-def meraki_helper(n):
-    """This will detect meraki numner"""
+def is_Meraki(n):
+    prev_digit = n%10
+    n = n//10
+    while n>0:
+        curr_digit = n%10
+        if abs(curr_digit-prev_digit)!=1:
+            return False
+        n = n//10;
+        prev_digit = curr_digit
+
+    return True;
+
 
 
 input = [12, 14, 56, 78, 98, 54, 678, 134, 789, 0, 7, 5, 123, 45, 76345, 987654321]
+
+Meraki_count = 0
+Not_Meraki_count = 0
+
+for num in input:
+    if is_Meraki(num):
+        print("Yes -", num, "is a Meraki Number")
+        Meraki_count += 1
+    else:
+        print("No  -", num, "is NOT a Meraki Number")
+        Not_Meraki_count += 1
+
+print("The input list contains",Meraki_count,"Meraki and", Not_Meraki_count,"non Meraki numbers")
+
