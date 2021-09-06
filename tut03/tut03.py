3c2434196fac9321867d0b455e931b29d0ea0441
ovd0312 <59286376+ovd0312@users.noreply.github.com>
2021-09-06 23:24:18 +0530

    Update tut03.py

--- tut03/tut03.py
+++ tut03/tut03.py
@@ -1,5 +1,7 @@
 import os.path
 
+#On Github, output_by_subject and output_individual_roll folders already have the output files
+
 #Function to create a comma-seperated string from string list
 #[1901EE43, 5, CS384, Open-Elective] -> 1901EE43,5,CS384,Open-Elective
 def create_string (row_items, relevant_cols):
@@ -14,18 +16,18 @@ def output(output_file, col_headings, row_items, relevant_cols):
 
     row_items = create_string(row_items,relevant_cols)
     col_headings = create_string(col_headings,relevant_cols)
-    if not os.path.isfile(output_file):   #If a file doesn't exist, create it
+    if not os.path.isfile(output_file):                     #If a file doesn't exist, create it
         with open(output_file,'x') as target_file:
-            target_file.write(col_headings)   # Put column headings
-    else:                                 #Append the row items to the file
+            target_file.write(col_headings)                 # Put column headings
+    else:                                                   #Append the row items to the file
         with open(output_file,'a') as target_file:
             target_file.write(row_items)
 
     return
 
-data_file = open("regtable_old.csv","r")        #Open the file
-col_headings = data_file.readline().split(',')  #Get the Top headings
-relevant_cols = [0,1,3,8]                       #Columns we care for
+data_file = open("regtable_old.csv","r")                    #Open the file
+col_headings = data_file.readline().split(',')              #Get the Top headings
+relevant_cols = [0,1,3,8]                                   #Columns we care for
 
 for curr_line in data_file:
     row_items = curr_line.split(',')
