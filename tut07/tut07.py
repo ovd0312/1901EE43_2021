cf572dad1394ca9338eae9da733ffe055a5f1cfb
ovd0312 <ovd0312@gmail.com>
2021-11-23 16:51:19 +0530

    Updated Solution for Tut7

--- tut07/tut07.py
+++ tut07/tut07.py
@@ -11,9 +11,8 @@ def get_ltp(string_ltp):
 	return ltp
 
 #Get all the expected feedbacks in a set
-def get_full_list(course_taken):
+def get_expected_fb(course_taken):
 	res_list = set()
-
 	for index, row in course_taken.iterrows():
 		course = row["subno"]
 		if is_rno(row["rollno"]):
@@ -23,11 +22,10 @@ def get_full_list(course_taken):
 				res_list.add((row["rollno"],row["subno"],"2"))
 			if course_ltp[course][2] != "0":
 				res_list.add((row["rollno"],row["subno"],"3"))
-
 	return res_list
 
 #Get all existing feedbacks in a set
-def get_done_list (fb_info):
+def get_recieved_fb (fb_info):
 	res_list = set()
 	for index, row in fb_info.iterrows():
 		if is_rno(row["stud_roll"]):
@@ -62,24 +60,25 @@ course_ltp = {}
 for index, row in course_master.iterrows():
 	course_ltp[row["subno"]] = get_ltp(row["ltp"])
 
-full_list = get_full_list(course_taken)
-done_list = get_done_list(fb_info)
+expected_fb = get_expected_fb(course_taken)
+recieved_fb = get_recieved_fb(fb_info)
 
 #Manage any discrepancy, if any
-full_list = full_list | done_list
+expected_fb = expected_fb | recieved_fb
 
-#Rem List = Dif in done_list and full_list
-rem_list = done_list ^ full_list
+#Rem List = Dif in recieved_fb and expected_fb
+remaining_fb = recieved_fb ^ expected_fb
 
 # Fill all the remaining feedback entries, 
 # along with other info, in a dataframe
-for entry in rem_list:
+reported = set()
+# Set for avoiding repetitions
+for entry in remaining_fb:
 	rno = entry[0]
 	course = entry[1]
 	feed_type = entry[2]
 	reg_sem = sch_sem[rno][course][0]
 	sched_sem = sch_sem[rno][course][1]
-
 	name = "NA_IN_STUDENTINFO"
 	mail = "NA_IN_STUDENTINFO"
 	amail = "NA_IN_STUDENTINFO"
@@ -89,9 +88,19 @@ for entry in rem_list:
 		mail = stud_info[rno]["email"]
 		amail = stud_info[rno]["aemail"]
 		contact = stud_info[rno]["contact"]
-	new_row = {"Roll Number":rno, "Registered Sem":reg_sem,"Scheduled Sem":sched_sem,"Course Code":course,"Email":mail,"AEmail":amail,"Contact":contact,"Name":name};
 		
-	output_file = output_file.append(new_row,ignore_index=True)
+	new_row = {"Roll Number":rno, 
+			   "Registered Sem":reg_sem,
+			   "Scheduled Sem":sched_sem,
+			   "Course Code":course,
+			   "Email":mail,
+			   "AEmail":amail,
+			   "Contact":contact,
+			   "Name":name};
+		
+	if (rno,course) not in reported:
+		output_file = output_file.append(new_row,ignore_index=True)
+	reported.add((rno,course))
 
 #Put Dataframe in xlsx
 output_file.to_excel("course_feedback_remaining.xlsx",index=False)
